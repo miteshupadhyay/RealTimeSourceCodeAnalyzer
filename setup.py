e53eb234726107eee214d640c601e39e8c1d055b
Mitesh Upadhyay <upadhyaymitesh91@gmail.com>
2026-05-02 19:43:13 +0530

    Setup File Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="real-time-source-code-analyzer",
+    version="0.1.0",
+    author="Mitesh Upadhyay",
+    author_email="Upadhyay.mitesh@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
